b04e144e99b122ab78b19bec63094afe1e89f0d8
Caleb Hemphill <calebhemp@gmail.com>
2023-12-20 16:37:22 -0500

    Add get/post user test

--- handlers/usersDB.js
+++ handlers/usersDB.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+let userTest;
 
+// routes
+// router.get('/all', getAllUsers);
+router.get('/:userId', getUser);
+router.post('/', createUser);
+// router.put('/:userId', updateUser);
+// router.delete('/:userId', deleteUser);
+
+// handlers
+async function getUser(request, response) {
+  const userId = request.params.userId;
+
+  if (userId === userTest.username) {
+    response.status(200).json(userTest);
+  } else {
+    response.status(400).send('User Not Found');
+  }
+
+}
+
+async function createUser(request, response) {
+  const user = request.body;
+  userTest = user;
+  response.status(201).json(userTest);
+}
 
 module.exports = router;
