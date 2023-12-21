befe5c5865f4a351ebc3605850823a6644532595
Caleb Hemphill <calebhemp@gmail.com>
2023-12-20 20:43:42 -0500

    Update user model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,10 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema(
   {
-    username: { type: String, default: '', unique: true }
+    username: { type: String, default: '', unique: true },
+    name: { type: String, default: '' },
+    favoriteLeague: { type: String, default: '' },
+    favoriteTeam: { type: String, default: '' }
   }
 );
 
